2e8e4a00948494a2edb727ab84b1bebb174ab8db
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-03-25 18:07:50 -0400

    basic framework finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,9 +11,24 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+      .then((res) => {
+        setPeople(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters List</h1>
+      { people.map((character, index) => {
+        return <Character char={character} key={index}/>;
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,56 @@
 // Write your Character component here
+
+import React from 'react';
+import styled from 'styled-components';
+
+
+const CharWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const CharDescWrapper = styled.div`
+    background: white;
+    padding: 10px; 
+`
+
+const Name = styled.h3`
+    background: white;
+    padding: 10px; 
+`;
+
+
+export default function Character(props) {
+    return (
+        <CharWrapper>
+            <Name>
+                {props.char.name}
+            </Name>
+        <CharDescWrapper>
+            <p>
+                Gender: {props.char.gender}
+            </p>
+            <p>
+                Height: {props.char.height}
+            </p>
+            <p>
+                Mass: {props.char.mass}
+            </p>
+            <p>
+                Birth Year: {props.char.birth_year}
+            </p>
+            <p>
+                Eye Color: {props.char.eye_color}
+            </p>
+            <p>
+                Hair Color: {props.char.hair_color}
+            </p>
+            <p>
+                Skin Color: {props.char.skin_color}
+            </p>
+        </CharDescWrapper>
+            
+        </CharWrapper>
+    )
+}
